93729c0e0ff11453b7fde9f5fcaa1f1df9af6b4b
Bharat Swamy <bh_swami@yahoo.com>
2023-03-09 17:04:50 +0530

    django authentication

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -134,3 +134,18 @@ STATICFILES_DIRS = [
 # media files configurations
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR /'media'
+
+
+from django.contrib.messages import constants as messages
+MESSAGE_TAGS = {
+    messages.ERROR: 'danger',
+}
+
+
+# SMTP configuration
+DEBUG = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'rbswamy10@gmail.com'
+EMAIL_HOST_PASSWORD = 'adcxrwgmbfvxsdsd'
+EMAIL_USE_TLS = True
